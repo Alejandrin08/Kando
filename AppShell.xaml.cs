98df50a02cc0c90b739a98340d87863f4d6269a3
Alejandrin08 <zs21013870@estudiantes.uv.mx>
2025-12-06 22:09:31 -0600

    first commit, desktop project created

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kando_desktop
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "kando_desktop")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "kando_desktop.Pages")]
